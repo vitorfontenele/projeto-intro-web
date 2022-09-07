6afe440f8546c060d4c1eb6fc7e129bfd5bda019
vitorfontenele <vitor_fontenele@hotmail.com>
2022-09-07 18:25:26 -0300

    Atividade da primeira semana

---
+++ script.js
@@ -0,0 +1,30 @@
+//Cursos
+/*
+const titulo; //string
+const autor; //string
+const preco; //number
+const horas; //number
+const possuiSelo; //boolean
+const topicos; //array (object)
+*/
+
+const titulo1 = "Aprenda inglês do zero";
+const autor1 = "Maria das Neves";
+const preco1 = 150;
+const horas1 = 50;
+const possuiSelo1 = true;
+const topicos1 = ["Inglês"];
+
+const titulo2 = "React com 3 projetos para o seu portfólio";
+const autor2 = "Gabriela da Silva";
+const preco2 = 200;
+const horas2 = 15;
+const possuiSelo2 = false;
+const topicos2 = ["Javascript", "HTML", "CSS", "React"];
+
+const titulo3 = "Como ganhar dinheiro com Marketing Digital";
+const autor3 = "Ana Ferreira";
+const preco3 = 50;
+const horas3 = 10;
+const possuiSelo3 = true;
+const topicos3 = ["Marketing Digital", "Redes Sociais"];
