05338ddbbdd53016aefe8589ab532cd9e624c5be
vitorfontenele <vitor_fontenele@hotmail.com>
2022-09-07 18:37:45 -0300

    Atualização da atividade da primeira semana

--- script.js
+++ script.js
@@ -28,3 +28,34 @@ const preco3 = 50;
 const horas3 = 10;
 const possuiSelo3 = true;
 const topicos3 = ["Marketing Digital", "Redes Sociais"];
+
+const mediaPrecos = (preco1 + preco2 + preco3)/3;
+console.log(mediaPrecos);
+
+const possuiSelos = possuiSelo1 && possuiSelo2 && possuiSelo3;
+console.log(possuiSelos);
+
+console.log("titulo: ", titulo1.toUpperCase());
+console.log("autor: ", autor1.toUpperCase());
+console.log("preco: ", preco1);
+console.log("horas: ", horas1);
+console.log("possuiSelo: ", possuiSelo1);
+console.log("topicos: ", topicos1);
+
+console.log("=================");
+
+console.log("titulo: ", titulo2.toUpperCase());
+console.log("autor: ", autor2.toUpperCase());
+console.log("preco: ", preco2);
+console.log("horas: ", horas2);
+console.log("possuiSelo: ", possuiSelo2);
+console.log("topicos: ", topicos2);
+
+console.log("=================");
+
+console.log("titulo: ", titulo3.toUpperCase());
+console.log("autor: ", autor3.toUpperCase());
+console.log("preco: ", preco3);
+console.log("horas: ", horas3);
+console.log("possuiSelo: ", possuiSelo3);
+console.log("topicos: ", topicos3);
